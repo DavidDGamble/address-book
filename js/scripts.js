30a71115b36c462f2c15ba69e5597ab8f0d8ec89
Student <student@epicodus-6.hsd1.or.comcast.net>
2022-10-31 13:31:53 -0700

    Add contact search to business logic in js.
    Co-authored-by: Robert Bryan <robbryandev@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,9 @@
 
 // Object constructors
 function AddressBook(contact) {
-  this.contact = contact;
-  this.currentId = 0;
+	this.contact = contact;
+	this.currentId = 0;
 }
-{}
 
 function Contact(firstName, lastName, phone) {
 	this.firstName = String(firstName);
@@ -14,24 +13,24 @@ function Contact(firstName, lastName, phone) {
 }
 
 // prototype methods
-AddressBook.prototype.addContact = function(contact) {
-  contact.id = this.assignId();
-  this.contact[contact.id] = contact;
+AddressBook.prototype.addContact = function (contact) {
+	contact.id = this.assignId();
+	this.contact[contact.id] = contact;
 };
 
-AddressBook.prototype.assignId = function() {
+AddressBook.prototype.assignId = function () {
 	this.currentId += 1;
 	return this.currentId;
 };
 
-AddressBook.prototype.findContact = function(id) {
-	if (this.contacts[id] !== undefined) {
-		return this.contacts[id];
-	} 
+AddressBook.prototype.findContact = function (id) {
+	if (this.contact[id]) {
+		return this.contact[id];
+	}
 	return false;
 };
 
-AddressBook.prototype.deleteContact = function(id) {
+AddressBook.prototype.deleteContact = function (id) {
 	let dContact = this.findContact(id);
 	if (dContact) {
 		delete dContact;
@@ -40,12 +39,23 @@ AddressBook.prototype.deleteContact = function(id) {
 	return false;
 };
 
-Contact.prototype.fullName = function() {
+Contact.prototype.fullName = function () {
 	return this.firstName + " " + this.lastName;
 };
 
+AddressBook.prototype.findName = function (nameArray) {
+	let searchContacts = [];
+	const addrs = this;
+	Object.keys(this.contact).forEach(function(nameIndex) {
+		if (nameArray.includes(addrs.contact[nameIndex].firstName) || nameArray.includes(addrs.contact[nameIndex].lastName)) {
+			searchContacts.push(addrs.contact[nameIndex]);
+		}
+	});
+	return searchContacts;
+};
+
 // Ui logic
-function handleForm(event) {
+function handleAddForm(event) {
 	event.preventDefault();
 	const firstName = document.getElementById('firstName').value;
 	const lastName = document.getElementById('lastName').value;
@@ -55,13 +65,22 @@ function handleForm(event) {
 	console.log(addressBook);
 }
 
+function handleSearchForm(event) {
+	event.preventDefault();
+	const search = document.getElementById("search").value;
+	const words = search.split(" ");
+	console.log(addressBook.findName(words));
+}
+
 function formSubmit() {
-	const form = document.querySelector("form");
-	form.addEventListener("submit", handleForm);
+	const addForm = document.getElementById("add-form");
+	addForm.addEventListener("submit", handleAddForm);
+	const searchForm = document.getElementById("search-form");
+	searchForm.addEventListener("submit", handleSearchForm);
 }
 
 // on load
-addEventListener("load", function() {
+addEventListener("load", function () {
 	addressBook = new AddressBook({});
 	formSubmit();
 });
